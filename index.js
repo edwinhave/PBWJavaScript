ed4a24c1511d1dbcd2619ede9ea506f73ea98b03
Edwin <108865852+edwinhave@users.noreply.github.com>
2023-01-27 18:55:49 +0700

    pertemuan1js

---
+++ index.js
@@ -0,0 +1,107 @@
+// console.log('Hello World');
+
+
+
+// let a = 10;
+// console.log(a++); //GAK AKAN NAMBAH APA-APA
+
+// let b = 10;
+// console.log(++b); //INI BARU NAMBAH
+
+// //STRING LITERAL
+// console.log(`Variabel a bernilai ${a}`);
+
+
+
+// let c = 10;
+// const d = 20;//GAK AKAN BISA BERUBAH UNTUK CONSTANTA
+
+// c *=2;
+// d *=3;
+
+// console.log(`Variabel a bernilai ${c}`);
+
+// ⬇️FUNCTION IF CONDITION
+let a = 2;
+let b = 3;
+// let c = new Number;
+
+// if(a>b){
+//     console.log('a is greater than b');
+// } else {console.log('a is not greater than b ');
+// }
+
+//DISARANKAN UNTUK MENGGUNAKAN === (SMAA DENGAN 3x) untuk mengecek value
+// if(a===b){ //SAMA DENGAN 3 ARTINYA ➡️ apakah 'a' identik dengan 'b'?
+//     console.log('sama nilainya');
+// } else {console.log('tidak sama ');
+// }
+
+// if(a==b){ //SAMA DENGAN 3 ARTINYA ➡️ apakah 'a' identik dengan 'b'?
+//     console.log('sama nilainya');
+// } else {console.log('tidak sama ');
+// }
+
+//📢Typeof
+// console.log(typeof c);
+
+//📢TIDAK DIPAKAI UNTUK CONDITIONAL ⬇️
+// if(a instanceof Number){
+//     console.log('a is a Number');
+// }
+
+// switch(true){//SWITCH LANGSUNG VALUE / VALUE YANG SPESIFIK
+//     case a==b:
+//         console.log('Ya');
+//         break;
+//     case a===b:
+//         console.log('No');
+//         break;
+//     }
+
+// switch(a){//APA ISI VALUENYA!
+//     case 1:
+//         console.log('Ya');
+//     case 1:
+//         console.log('Ya');
+//     Default;
+//     console.log('Default');
+// }
+
+// const c = a == b ? 2 : 3;
+// console.log(c);
+// console.log(a == b ? 'true': 'salah');
+// //(Kondisi ? 'Value Kalau isinya "True"' : 'Value kalau isinya tidak memenuhi"False"')
+
+// //LOOPING
+// for (let i = 0; i<10;i++) {
+//     if (i % 2 == 0)
+//     console.log(`${i} Genap`);
+// }
+
+// //WHILE
+// let i =0;
+// while(i<20){
+//     console.log(i);
+//     i++;
+// }
+
+//DO & WHILE
+
+//BREAK & CONTINUE
+// let i = 0;
+// while (i < 10){
+//     i++
+//     if(i === 5){
+//         continue
+//     }
+
+//     alert("Hello World");
+
+// }
+
+    let z = 100;
+    while(z <=200){
+        console.log(z)
+        z +=20
+    }
